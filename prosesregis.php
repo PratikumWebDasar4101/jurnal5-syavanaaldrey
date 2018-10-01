6f170469986d5b77fa3c2f974ee6cde2a1e9b8f3
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-01 10:47:29 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -2,23 +2,23 @@
 $nim		= $_POST['nim'];
 		$nama		= $_POST['nama'];
 		$email		= $_POST['email'];
-		$reinput	= "<br><a href ='registrasi.php'>RE-INPUT</a>";
-		$enter		= "<br>";
+		$iputagain	= "<br><a href ='registrasi.php'>INPUT ULANG</a>";
+		$line		= "<br>";
 
 		if(strlen($nama) > 10){
 			echo "Max length 25 word";
-			echo $reinput;
-			echo $enter;
+			echo $inputagain;
+			echo $line;
 		}
 		if(!is_numeric($nim) || strlen((string)$nim) > 10){
 			echo "NIM harus angka dan max lenth 10";
-			echo $reinput;
-			echo $enter;
+			echo $inputagain;
+			echo $line;
 		}
 		if(!preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z.]{2,5}$/",$email)){
 			echo "Format salah";
-			echo $reinput;
-			echo $enter;
+			echo $inputagain;
+			echo $line;
 		}else{
 			header("Location: komentar.php");
 		}				
