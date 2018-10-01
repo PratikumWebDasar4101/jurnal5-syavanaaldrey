c28b28513bb7c53636511fd641d44dcea6204dc2
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-01 10:39:49 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -4,8 +4,35 @@
 	<title>Form Registrasi</title>
 </head>
 <body>
-
-
-
+<form action="prosesregis.php" method="POST">
+		<table>
+			<tr>
+				<td>
+					NIM
+				</td>
+				<td>
+					<input type="text" name="nim">
+				</td>
+			<tr>
+				<td>
+					NAMA
+				</td>
+				<td>
+					<input type="text" name="nama">
+				</td> 
+			<tr>
+				<td>
+					E-MAIL
+				</td>
+				<td>
+					<input type="text" name="email">
+				</td>
+			<tr>
+				<td>
+					<input type="submit" name="kirim" value="KIRIM">
+				</td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
