e58641653d77088dfd9ad4a47f2281634d8676e0
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-10-01 10:38:19 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,14 @@
 <?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
-
-
-
-
+<html>
+<head>
+	<title>KOMENTAR</title>
+</head>
+<body>
+	<form method="POST" action="hasilcom.php">
+	SILAHKAN INPUT KOMENTAR DISINI : <br>
+	<textarea rows="10" cols="30" name="komentar"></textarea><br>
+	<input type="submit" name="kirim" value="KIRIM">
+	</form>
+</body>
+</html>
 ?>
